37b3ac5ea74d3ae5e862eba1dee7e550d864bb2a
Kathy_M <katherine.munoz.moran@gmail.com>
2025-11-05 15:08:35 +0100

    partie meteo fini

--- main.js
+++ main.js
@@ -16,6 +16,28 @@ const affichageTache = document.getElementById("tache-display");
 const tacheInput = document.getElementById("tache-input");
 const btnSauvegarder = document.getElementById("sauvegarder-btn");
 
-affichageTache.
 
 
+
+
+
+const affichageMeteo = document.getElementById("meteo-display");//Je récupère mon id dans une variable
+function addMeteo(texte) {//Création d'une fonction pour insérer le texte json
+    affichageMeteo.innerText = texte;
+}
+
+  fetch("https://prevision-meteo.ch/services/json/toulouse")//lance une requête HTTP GET vers l'URL fournie et renvoie une promesse qui sera résolue quand la réponse arrive.
+      .then(response => {
+          return response.json();
+      })
+      .then(json => {
+          let texte = `
+          Température : ${json.current_condition.tmp} °
+          Vitesse du Vent : ${json.current_condition.wnd_spd}
+          Description : ${json.current_condition.condition}
+          `;
+          addMeteo(texte);
+      })
+      .catch(error =>{
+          addMeteo("Erreur l\' api ne répond pas");
+      })
